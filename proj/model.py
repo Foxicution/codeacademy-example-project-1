f0d679b2f7175cebcf8ad907e81cf0e2e920117d
Foxicution <foxicution@gmail.com>
2024-10-28 20:18:44 +0000

    implemented authentication and session management

--- proj/model.py
+++ proj/model.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass, field
-from datetime import datetime
-from typing import Any, Literal
+from datetime import datetime, timedelta
+from hashlib import sha256
+from typing import Any
 from uuid import UUID, uuid4
+
+SESSION_DURATION = timedelta(hours=1)
 
 
 class Ok(str): ...
@@ -55,11 +58,22 @@ class Reader:
     id: UUID = field(default_factory=uuid4)
 
 
-@dataclass
 class Librarian:
-    username: str
-    password: str
-    id: UUID = field(default_factory=uuid4)
+    def __init__(self, username: str, password: str):
+        self.id = uuid4()
+        self.username = username
+        # TODO: Could use a more secure hashing algorithm here
+        self.password_hash = sha256(password.encode()).hexdigest()
+
+    def auth(self, password: str) -> bool:
+        return self.password_hash == sha256(password.encode()).hexdigest()
+
+
+class Session:
+    def __init__(self, user: Librarian | Reader):
+        self.user = user
+        self.expiration = datetime.now() + SESSION_DURATION
+        self.id = uuid4()
 
 
 @dataclass
@@ -69,11 +83,73 @@ class Model:
     books: dict[UUID, Book] = field(default_factory=dict)
     borrows: dict[UUID, Borrow] = field(default_factory=dict)
 
+    sessions: dict[UUID, Session] = field(default_factory=dict)
+
+    def __cleanup_sessions(self):
+        now = datetime.now()
+        expired_sessions = [
+            session_id
+            for session_id, session in self.sessions.items()
+            if session.expiration < now
+        ]
+        
+        for session in expired_sessions:
+            self.sessions.pop(session.id)
+
+    def add_librarian(self, username: str, password: str) -> Ok | Err:
+        if any(
+            librarian.username == username for librarian in self.librarians.values()
+        ):
+            return Err(f"Librarian with username: {username} already exists.")
+
+        librarian = Librarian(username, password)
+        self.librarians[librarian.id] = librarian
+        return Ok(f"Librarian {username} succesfully added.")
+
+    def auth_librarian(self, username: str, password: str) -> UUID | Err:
+        librarian = next(
+            (
+                librarian
+                for librarian in self.librarians.values()
+                if librarian.username == username and librarian.auth(password)
+            ),
+            None,
+        )
+        if librarian is None:
+            return Err("No librarian matching the username and password found.")
+
+        session = Session(librarian)
+        self.sessions[session.id] = session
+        self.__cleanup_sessions()
+        return session.id
+
     def add_reader(self, reader: Reader) -> Ok | Err:
         if reader.id in self.readers.keys():
             return Err(f"A reader with id {reader.id} exists.")
         self.readers[reader.id] = reader
-        return Ok(f"Reader {reader.name} succesfully created.")
+        return Ok(
+            f"Reader {reader.name} succesfully created. Reader card number: {reader.id.int}"
+        )
+
+    def auth_reader(self, reader_id: UUID) -> UUID | Err:
+        if reader_id not in self.readers:
+            return Err("No reader found with this card number.")
+
+        session = Session(self.readers[reader_id])
+        self.sessions[session.id] = session
+        self.__cleanup_sessions()
+        return session.id
+    
+    def validate_session(self, session_id: UUID) -> Librarian | Reader | Err:
+        if session_id not in self.sessions:
+            return Err("Session is invalid")
+        
+        session = self.sessions[session_id]
+        if datetime.now() > session.expiration:
+            self.sessions.pop(session.id)
+            return Err("Session is expired")
+        
+        return self.sessions[session_id].user
 
     def add_book(self, book: Book) -> Ok | Err:
         if any(book == existing_book for existing_book in self.books.values()):
